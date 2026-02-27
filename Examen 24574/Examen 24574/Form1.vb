0ac022c6e504969761a0eb559267c39ef85a2dd0
mandil01 <juan.castro03@iest.edu.mx>
2026-02-27 08:13:48 -0600

    Delete Examen 24574 directory

--- Examen 24574/Examen 24574/Form1.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim x As Double = Double.Parse(TextBox1.Text)
-        Dim y As Double = Double.Parse(TextBox2.Text)
-        Dim r As Double = (x + y) / 2
-        Label4.Text = "El promedio es" & " " & r.ToString
-    End Sub
-End Class
