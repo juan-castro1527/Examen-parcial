12ede0e08dbc0cee51e22d719ecb5baed9f374b1
mandil01 <juan.castro03@iest.edu.mx>
2026-02-27 07:42:09 -0600

    Mi examen

---
+++ Examen 24574/Examen 24574/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x As Double = Double.Parse(TextBox1.Text)
+        Dim y As Double = Double.Parse(TextBox2.Text)
+        Dim r As Double = (x + y) / 2
+        Label4.Text = "El promedio es" & " " & r.ToString
+    End Sub
+End Class
