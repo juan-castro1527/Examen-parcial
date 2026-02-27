d01309fa59de8ac724ec8f2d34af23ad3b0e21f6
mandil01 <juan.castro03@iest.edu.mx>
2026-02-27 08:14:12 -0600

    Add files via upload

---
+++ Examen-parcial-main/Examen 24574/Examen 24574/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            Label4.Text = "Faltan datos"
+        Else
+            Dim x As Double = Double.Parse(TextBox1.Text)
+            Dim y As Double = Double.Parse(TextBox2.Text)
+            Dim r As Double = (x + y) / 2
+            Label4.Text = "El promedio es" & " " & r.ToString
+        End If
+
+    End Sub
+
+
+End Class
